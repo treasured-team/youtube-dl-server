bb5bb431d2ebf056a8144bab6dff2e2994c9b2e4
NikitaVr <nvarabei@gmail.com>
2021-02-22 16:18:00 -0500

    update push image check

--- pushImage.sh
+++ pushImage.sh
@@ -3,7 +3,7 @@ then
     docker login -u unset -p $OKD_DEV_REGISTRY_TOKEN $OKD_DEV_REGISTRY_URL
     docker tag image $OKD_DEV_REGISTRY_URL/treasureworld/youtube-dl-server:latest
     docker push $OKD_DEV_REGISTRY_URL/treasureworld/youtube-dl-server
-elif [ $LAYERCI_BRANCH = 'main' ]
+elif [ $LAYERCI_BRANCH = 'master' ]
 then
     docker login -u unset -p $OKD_PRODUCTION_REGISTRY_TOKEN $OKD_PRODUCTION_REGISTRY_URL
     docker tag image $OKD_PRODUCTION_REGISTRY_URL/treasureworld/youtube-dl-server:latest
